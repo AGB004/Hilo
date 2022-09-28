1875a1ed39c33b2e1954f3f1bc9979f990a46447
AGB004 <alecbyington@gmail.com>
2022-09-27 18:39:58 -0600

    update

--- cards/game/cards.py
+++ cards/game/cards.py
@@ -1,57 +1,23 @@
 import random
 
-
-# TODO: Implement the Die class as follows...
-
-# 1) Add the class declaration. Use the following class comment.
 class Cards():
-    """A small cube with a different number of spots on each of its six sides.
-
-    The responsibility of Die is to keep track of the side facing up and calculate the points for 
-    it.
-   
-    Attributes:
-        value (int): The number of spots on the side facing up.
-        points (int): The number of points the die is worth.
-    """
-
-
-    # 2) Create the class constructor. Use the following method comment.
+    
     def __init__(self):
-        """Constructs a new instance of Die with a value and points attribute.
-
-        Args:
-            self (Die): An instance of Die.
-        """
+       
         self.value1 = 0
         self.value2 = 0
         self.points = 0
 
-    # 3) Create the roll(self) method. Use the following method comment.
     def draw1(self):
-        """Generates a new random value and calculates the points.
         
-        Args:
-            self (Die): An instance of Die.
-        """
         self.value1 = random.randint(1, 13)
     
-    # 3) Create the roll(self) method. Use the following method comment.
     def draw2(self):
-        """Generates a new random value and calculates the points.
         
-        Args:
-            self (Die): An instance of Die.
-        """
         self.value2 = random.randint(1, 13)
 
-    # 3) Create the roll(self) method. Use the following method comment.
     def calculate_hi(self):
-        """Generates a new random value and calculates the points.
-        
-        Args:
-            self (Die): An instance of Die.
-        """    
+          
         if self.value1 < self.value2:
             self.points = 100
         elif self.value1 > self.value2:
@@ -59,13 +25,8 @@ class Cards():
         else:
             self.points = 0
 
-    # 3) Create the roll(self) method. Use the following method comment.
     def calculate_lo(self):
-        """Generates a new random value and calculates the points.
-        
-        Args:
-            self (Die): An instance of Die.
-        """    
+         
         if self.value1 > self.value2:
             self.points = 100
         elif self.value1 < self.value2:
--- cards/game/director.py
+++ cards/game/director.py
@@ -1,24 +1,9 @@
 from game.cards import Cards
 
-
 class Director:
-    """A person who directs the game. 
     
-    The responsibility of a Director is to control the sequence of play.
-
-    Attributes:
-        dice (List[Die]): A list of Die instances.
-        is_playing (boolean): Whether or not the game is being played.
-        score (int): The score for one round of play.
-        total_score (int): The score for the entire game.
-    """
-
     def __init__(self):
-        """Constructs a new Director.
         
-        Args:
-            self (Director): an instance of Director.
-        """
         self.card1 = []
         self.card2 = []
         self.is_playing = True
@@ -34,31 +19,19 @@ class Director:
             self.card2.append(card2)
 
     def start_game(self):
-        """Starts the game by running the main game loop.
         
-        Args:
-            self (Director): an instance of Director.
-        """
         while self.is_playing:
             self.draw_cards()
             self.do_outputs()
             self.get_play()
 
     def get_play(self):
-        """Ask the user if they want to roll.
-
-        Args:
-            self (Director): An instance of Director.
-        """
+        
         play = input("Play again? [y/n] ")
         self.is_playing = (play == "y")
 
     def draw_cards(self):
-        """Updates the player's score.
-
-        Args:
-            self (Director): An instance of Director.
-        """
+        
         if not self.is_playing:
             return 
 
@@ -94,11 +67,7 @@ class Director:
             self.total_score += self.score
 
     def do_outputs(self):
-        """Displays the dice and the score. Also asks the player if they want to roll again. 
-
-        Args:
-            self (Director): An instance of Director.
-        """
+        
         print(f"Your total score is: {self.total_score}\n")
         self.is_playing == (self.score > 0)
         
